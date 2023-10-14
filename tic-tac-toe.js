bd57b92254abf977bac29e8144cb348161d3f0bc
Dominick Powell <dombert123@gmail.com>
2023-10-14 16:33:49 -0500

    Edited js file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,15 @@
-window.onload = function(){
-    const game=document.getElementById("game");
-    const status=document.getElementById("status");
-    const board=document.getElementById("board");
-    const controls=document.getElementsByClassName("controls");
-    const button=document.getElementsByClassName("btn")[0];
+document.addEventListener('DOMContentLoaded', function() {
+    const game = document.getElementById("game");
+    const status = document.getElementById("status");
+    const board = document.getElementById("board");
+    const controls = document.getElementsByClassName("controls");
+    const button = document.getElementsByClassName("btn")[0];
 
-    const e= board.querySelectorAll('div');
-    let play = 'X';
-    let poslst=['', '', '', '', '', '', '', '', ''];
+    const squares = board.querySelectorAll('div');
+    let currentPlayer = 'X';
+    let positions = ['', '', '', '', '', '', '', '', ''];
 
-    const wincrit= [
+    const winCriteria = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
@@ -20,74 +20,61 @@ window.onload = function(){
         [2, 4, 6]
     ];
 
+    // Set up the board
+    squares.forEach((square, index) => {
+        square.classList.add("square");
 
-    for (let z=0; z<=8; z++){
-        e[z].setAttribute("class", "square");
-
-    }
-
-    e.forEach( (element, index) => {
-        element.addEventListener('click', () => user(element, index));
-        element.addEventListener('mouseover', function(){
-            element.classList.add('hover');
-        });
-        element.addEventListener('mouseout', function(){
-            element.classList.remove('hover');
-        });
+        square.addEventListener('click', () => handleUserMove(square, index));
+        square.addEventListener('mouseover', () => square.classList.add('hover'));
+        square.addEventListener('mouseout', () => square.classList.remove('hover'));
     });
 
-    function verifywin(){
-        for(let w=0; w<=7; w++){
-            const win = wincrit[w];
-    
-            const pos1= poslst[win[0]];
-            const pos2 = poslst[win[1]];
-            const pos3 = poslst[win[2]];
-            if (pos1 === ''|| pos2 === ''|| pos3===''){
+    // Handle user move
+    function handleUserMove(square, index) {
+        if (square.innerText !== 'X' && square.innerText !== 'O') {
+            square.innerText = currentPlayer;
+            square.classList.add(currentPlayer);
+            positions[index] = currentPlayer;
+
+            // Check for a win after each move
+            checkForWin();
+
+            // Switch players
+            currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+        }
+    }
+
+    // Check for a win
+    function checkForWin() {
+        for (let w = 0; w <= 7; w++) {
+            const win = winCriteria[w];
+
+            const pos1 = positions[win[0]];
+            const pos2 = positions[win[1]];
+            const pos3 = positions[win[2]];
+
+            if (pos1 === '' || pos2 === '' || pos3 === '') {
                 continue;
-            }  
-            if (pos1=== pos2 && pos2 === pos3){
-                status.innerHTML= 'Congratulations! ' + pos1 +' is the winner';                
+            }
+
+            if (pos1 === pos2 && pos2 === pos3) {
+                status.innerHTML = `Congratulations! ${pos1} is the winner`;
                 status.classList.add('you-won');
                 break;
-            }  
-
-        }
-    }
-        
-    const user= (element,index) =>{
-        console.log(element.innerText)
-        if(element.innerText !== 'X' || element.innerText !== 'O'){
-            element.innerText= play;
-            element.classList.add(play);
-            poslst[index]=play;
-            console.log(poslst);
-            verifywin();
-            if(play === 'X'){
-                play='O';
-            }
-            else
-            {
-                play='X';
             }
         }
-
     }
 
-    
-
-    
-    
-    button.addEventListener('click', ()=>{
-        poslst=['', '', '', '', '', '', '', '', ''];
-        status.innerHTML= 'Move your mouse over a square and click to play an X or an O.'
+    // Reset the game
+    button.addEventListener('click', () => {
+        positions = ['', '', '', '', '', '', '', '', ''];
+        status.innerHTML = 'Move your mouse over a square and click to play an X or an O.';
         status.classList.remove('you-won');
-        e.forEach(element =>{
-            element.innerText ='';
-            element.classList.remove('X');
-            element.classList.remove('O');
+        squares.forEach(square => {
+            square.innerText = '';
+            square.classList.remove('X', 'O');
         });
+        currentPlayer = 'X';
     });
 
-    
-}
+});
